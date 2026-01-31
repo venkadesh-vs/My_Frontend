8d52589a5f7a146798598fd3e622cd84571a41e7
Venkadesh Sakthivel <vesakthivel@fssa.freshworks.com>
2026-01-31 12:19:29 +0530

    frontend: update api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://deployment-backend-5y18.onrender.com";
 
 // Helper to check if user is logged in
 function checkAuth() {
