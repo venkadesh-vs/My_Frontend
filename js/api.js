6a05e0d91409826fac5bcdee0d998abdd28e280d
Venkadesh Sakthivel <vesakthivel@fssa.freshworks.com>
2026-03-12 12:01:08 +0530

    new changes

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = "http://localhost:8000";
-// const API_BASE_URL = "https://my-backend-pwnq.onrender.com";
+// const API_BASE_URL = "http://localhost:8000";
+const API_BASE_URL = "https://my-backend-pwnq.onrender.com";
 
 // === Core Utility Functions ===
 
